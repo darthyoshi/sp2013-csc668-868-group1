500fbd6fe23ae8ec6ff5dd8178979027d496db79
lumberjasontwo19@gmail.com <lumberjasontwo19@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-02-11 07:10:17 +0000

    Expanding on TransactionReader.java with a simple exception

--- post1team2/POST1/src/post/client/view/TransactionReader.java
+++ post1team2/POST1/src/post/client/view/TransactionReader.java
@@ -1,42 +1,91 @@
 package post.client.view;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import post.client.controller.TransactionBuilder;
-import post.model.Transaction;
-
+import post.model.*;
 
 /**
  * Class TransactionReader
  */
 public class TransactionReader {
-    private BufferedReader reader;
 
-    /**
-     * Construct a new TransactionReader. This will read transactions from the 
-     * specified stream. 
-     * @param r 
-     */
+    private BufferedReader reader;
+    private List<String> nextBlock;
+
+    //
+    // Constructors
+    //
     public TransactionReader(Reader r) {
+        reader = new BufferedReader(r);
+        prepareBlock();
     }
 
     /**
-     * Check whether or not any transactions are remaining.
-     * @return boolean true if more transactions are available; otherwise false
+     * @return boolean
      */
     public boolean hasMoreTransactions() {
-        return false;
+        return !nextBlock.isEmpty() && nextBlock.size() > 2;
     }
 
     /**
-     * Get the next transaction from the stream. A TransactionBuilder is 
-     * provided to assist in assembling the transaction (this builder may 
-     * connect back to a POST in order to perform validation, for instance to
-     * confirm that UPCs are valid.)
      * @return Transaction
      * @param builder
      */
     public Transaction getNextTransaction(TransactionBuilder builder) {
-        return null;
+        builder.setCustomer(nextBlock.get(0).trim());
+        for (int i = 1; i < nextBlock.size() - 1; i++) {
+            String lineItem = nextBlock.get(i);
+            String upc = lineItem.substring(0, 4);
+            int quantity = 1;
+            if (lineItem.length() > 10) { // ...contains explicit quantity
+                quantity = Integer.parseInt(lineItem.substring(10));
+            }
+            builder.addLineItem(upc, quantity);
+        }
+        Payment p = parsePayment(
+                nextBlock.get(nextBlock.size()-1), builder.getAmountDue());
+        prepareBlock();
+        return builder.completeSale(p);
+    }
+
+    private Payment parsePayment(String line, float amountDue) {
+        
+        int x = 1;
+        
+        do{
+        try{
+        if (line.startsWith("CASH")) {
+            float amount = Float.parseFloat(line.substring(line.indexOf('$') + 1));
+            return new CashPayment(amount, amountDue);
+        } else if (line.startsWith("CREDIT")) {
+            long ccNumber = Long.parseLong(line.substring(line.indexOf(' ') + 1));
+            return new CreditPayment(amountDue, ccNumber);
+        } else if (line.startsWith("CHECK")) {
+            return new CheckPayment(amountDue);
+        }
+        }
+        
+        catch(Exception e){
+               System.out.println("Error: Specify payment type.");
+        }
+        }while(x==1);
+        
+        return null; 
+    }
+    
+    private void prepareBlock() {
+        nextBlock = new ArrayList<String>();
+        String nextLine;
+        try {
+            while ((nextLine = reader.readLine()) != null) {
+                if (nextLine.trim().isEmpty()) return; // Stop after newline
+                nextBlock.add(nextLine);
+            }
+        } catch (IOException ioe) {
+        }
     }
 }
