32e663b36d170735e01436b860ab57b88165e69f
vwoeltjen@gmail.com <vwoeltjen@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-02-11 18:30:16 +0000

    Implemented file-based cashier

--- post1team2/POST1/src/post/client/view/FileBasedCashier.java
+++ post1team2/POST1/src/post/client/view/FileBasedCashier.java
@@ -1,13 +1,20 @@
 package post.client.view;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import post.client.controller.POST;
+import post.model.Receipt;
+import post.model.Transaction;
 
 /**
- * Class FileBasedCashier
+ * A file-based cashier simulates the cashier's activity by reading
+ * transactions from a file and issuing them back to the POST.
+ *
+ * @author woeltjen
  */
 public class FileBasedCashier implements CashierView {
+    private static final String DEFAULT_TRANSACTIONS = "transaction.txt";
     private TransactionReader reader;
 
     /**
@@ -16,12 +23,25 @@ public class FileBasedCashier implements CashierView {
      * @throws IOException 
      */
     public FileBasedCashier() throws IOException {
+        this(new File(DEFAULT_TRANSACTIONS));
     }
 
+    /**
+     * Create a new "Cashier" that reads from a specified file"
+     * @param f the file containing formatted transaction information
+     * @throws IOException
+     */
     public FileBasedCashier(File f) throws IOException {
+        this(new TransactionReader(new FileReader(f)));
     }
 
+    /**
+     * Create a new "Cashier" that reads transactions from the
+     * specified TransactionReader
+     * @param reader the source for reading transactions
+     */
     public FileBasedCashier(TransactionReader reader) {
+        this.reader = reader;
     }
 
     /**
@@ -31,5 +51,10 @@ public class FileBasedCashier implements CashierView {
      * @param post the POST we are connected to
      */
     public void connectTo(POST post) {
+        while (reader.hasMoreTransactions()) {
+            Transaction t = reader.getNextTransaction(post.startTransaction());
+            Receipt     r = post.recordTransaction(t);
+            System.out.println(r.toColumnOutput() + "\n");
+        }
     }
 }
