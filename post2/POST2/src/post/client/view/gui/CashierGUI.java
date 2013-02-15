933c5241bb6574531f6c035eee23374f70113bdc
vwoeltjen@gmail.com <vwoeltjen@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-02-15 01:02:50 +0000

    When cashier GUI is created, do so on EDT. Also, prohibit resize.

--- post2/POST2/src/post/client/view/gui/CashierGUI.java
+++ post2/POST2/src/post/client/view/gui/CashierGUI.java
@@ -1,6 +1,8 @@
 package post.client.view.gui;
 
+import java.lang.Runnable;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import post.client.controller.POST;
 import post.client.view.CashierView;
 
@@ -11,11 +13,19 @@ import post.client.view.CashierView;
 public class CashierGUI implements CashierView {
 
     @Override
-    public void connectTo(POST post) {
-        JFrame frame = new CashierFrame(new GUIMediator(post));
-        frame.setVisible(true);
-        frame.pack();        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void connectTo(final POST post) {
+        // NOTE: Builds GUI on the EDT  
+        SwingUtilities.invokeLater(
+            new Runnable() {
+                @Override
+                public void run() {
+                    JFrame frame = new CashierFrame(new GUIMediator(post));                    
+                    frame.pack();
+                    frame.setVisible(true);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setResizable(false);
+                }
+            }
+        );
     }
-    
 }
