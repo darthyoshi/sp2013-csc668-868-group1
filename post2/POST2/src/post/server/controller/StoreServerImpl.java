fec9562cef01e8d4b63e605ae02478fb2b078314
steven.gimeno@gmail.com <steven.gimeno@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-02-20 21:37:20 +0000

    Last upload was a fluke.

--- post2/POST2/src/post/server/controller/StoreServerImpl.java
+++ post2/POST2/src/post/server/controller/StoreServerImpl.java
@@ -1,13 +1,51 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package post.server.controller;
 
+import post.model.ProductCatalog;
+import post.model.Receipt;
+import post.model.StoreDescription;
+import post.model.Transaction;
+
 /**
+ * Provides a general implementation of a store.
  *
- * @author academic
+ * This simply connects the Store's interface to a specific group of relevant
+ * objects provided during the constructor call.
+ * 
+ * @author steven
  */
-public class StoreServerImpl {
-    
+public class StoreServerImpl implements TransactionReceiver, Store {
+    private StoreDescription storeDescription;
+    private SalesLog salesLog;
+    private ProductCatalog catalog;
+    private TransactionReceiver paymentProcessor;
+
+    /**
+     * Create a new store.
+     * @param storeDescription a description of the store
+     * @param salesLog the saleslog the store should write to
+     * @param catalog a catalog of products available through the store.
+     * @param paymentProcessor returns a receipt object or null if the payment is incorrect or declined
+     */
+    public StoreServerImpl(StoreDescription storeDescription, SalesLog salesLog, ProductCatalog catalog, PaymentProcessor paymentProcessor) {
+        this.storeDescription = storeDescription;
+        this.salesLog = salesLog;
+        this.catalog = catalog;
+        this.paymentProcessor = paymentProcessor;
+    }
+
+    @Override
+    public StoreDescription getDescription() {
+        return storeDescription;
+    }
+
+    @Override
+    public ProductCatalog getCatalog() {
+        return catalog;
+    }
+
+    @Override
+    public Receipt recordTransaction(Transaction transaction) {
+        
+        return salesLog.recordTransaction(transaction);
+    }
 }
