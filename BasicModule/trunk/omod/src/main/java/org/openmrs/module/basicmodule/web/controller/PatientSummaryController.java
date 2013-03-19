6d66b866b792516a4554d49cb9e09c8a418a3128
vwoeltjen@gmail.com <vwoeltjen@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-03-19 15:54:17 +0000

    Fix compilation errors in patient summary controller

--- BasicModule/trunk/omod/src/main/java/org/openmrs/module/basicmodule/web/controller/PatientSummaryController.java
+++ BasicModule/trunk/omod/src/main/java/org/openmrs/module/basicmodule/web/controller/PatientSummaryController.java
@@ -4,6 +4,7 @@
  */
 package org.openmrs.module.basicmodule.web.controller;
 
+import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
-import org.openmrs.api.PatientService;
 import org.openmrs.Encounter;
-import org.openmrs.api.EncounterService;
+import org.openmrs.Person;
 
 @Controller
 @RequestMapping(value = "module/basicmodule/patientsummarylink.form")
@@ -32,26 +32,28 @@ public class PatientSummaryController {
 	}
     
     public String getHTML(String patientID) {
-        Patient p = PatientService.getPatient(method.valueOf("patientId"));
+        Patient p = Context.getPatientService().getPatient(Integer.parseInt(patientID));
         List<Encounter> encounters = Context.getEncounterService().getEncountersByPatient(p);
-        Person person = p.getPerson();
+        //Person person = p.getPerson(); Patient is-a person
+        
+        // TODO: Maybe move this to jsp and just prepare variable names here?
         
         String html = "<table width=100%>\n"+
                 "    <tr bgcolor='4D9999'>\n"+
                 "        <td colspan=2><font color='white'>Patient Information</font></td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td width=30% ><b>Name:</b> "+person.getPersonName().getFullName()+"</td>\n"+
-                "        <td width=70% align='right'><b>Clinic Location:</b> "+%patientCity%+"</td>\n"+
+                "        <td width=30% ><b>Name:</b> "+p.getPersonName().getFullName()+"</td>\n"+
+                "        <td width=70% align='right'><b>Clinic Location:</b> "+"%patientCity%"+"</td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td>"+person.getGender()+", "+person.getAge()+" yrs<td>\n"+
+                "        <td>"+p.getGender()+", "+p.getAge()+" yrs<td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td colspan=2><center><b>Address:</b> "+%patientAddress%+"</center>\n"+
+                "        <td colspan=2><center><b>Address:</b> "+"%patientAddress%"+"</center>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td colspan=2><center><b>Last seen on:</b> "+%LastSeen%+"</center>\n"+
+                "        <td colspan=2><center><b>Last seen on:</b> "+"%LastSeen%"+"</center>\n"+
                 "    </tr>\n"+
                 "</table>\n"+
                 "\n"+
@@ -62,15 +64,15 @@ public class PatientSummaryController {
                 "        <td colspan=2><font color='white'>Patient Summary</font></td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td width=30% ><b>WHO stage:</b> "+%patientWHOStage%+"</td>\n"+
+                "        <td width=30% ><b>WHO stage:</b> "+"%patientWHOStage%"+"</td>\n"+
                 "        <td width=70% ><b>Current art regimen drugs:</b> </td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td width=30% ><b>TB Status:</b> "+%TBStatus%+"</td>\n"+
-                "        <td width=70% rowspan='2'><b>"+%ARTRegimen%+"</b> </td>\n"+
+                "        <td width=30% ><b>TB Status:</b> "+"%TBStatus%"+"</td>\n"+
+                "        <td width=70% rowspan='2'><b>"+"%ARTRegimen%"+"</b> </td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td width=30% ><b>Allergies:</b> "+%patientAllergies%+"</td>\n"+
+                "        <td width=30% ><b>Allergies:</b> "+"%patientAllergies%"+"</td>\n"+
                 "    </tr>\n"+
                 "</table>\n"+
                 "\n"+
@@ -89,18 +91,18 @@ public class PatientSummaryController {
                 "        <td ><b>Karnofsky Score</b></td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td >Enrollment: "+%enroll%+"</td>\n"+
-                "        <td >"+%enrollWt%+"</td>\n"+
-                "        <td >"+%enrollTemp%+"</td>\n"+
-                "        <td >"+%enrollBP%+"</td>\n"+
-                "        <td >"+%enrollKS%+"</td>\n"+
+                "        <td >Enrollment: "+"%enroll%"+"</td>\n"+
+                "        <td >"+"%enrollWt%"+"</td>\n"+
+                "        <td >"+"%enrollTemp%"+"</td>\n"+
+                "        <td >"+"%enrollBP%"+"</td>\n"+
+                "        <td >"+"%enrollKS%"+"</td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td >Last Visit: "+%lastVisit%+"</td>\n"+
-                "        <td >"+%lastVisitWt%+"</td>\n"+
-                "        <td >"+%lastVisitTemp%+"</td>\n"+
-                "        <td >"+%lastVisitBP%+"</td>\n"+
-                "        <td >"+%lastVisitKS%+"</td>\n"+
+                "        <td >Last Visit: "+"%lastVisit%"+"</td>\n"+
+                "        <td >"+"%lastVisitWt%"+"</td>\n"+
+                "        <td >"+"%lastVisitTemp%"+"</td>\n"+
+                "        <td >"+"%lastVisitBP%"+"</td>\n"+
+                "        <td >"+"%lastVisitKS%"+"</td>\n"+
                 "    </tr>\n"+
                 "</table>\n"+
                 "\n"+
@@ -117,9 +119,9 @@ public class PatientSummaryController {
                 "        <td ><b>Viral Load</b></td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "        <td >"+%LabCD4%+"</td>\n"+
-                "        <td >"+%LabHemoglobin%+"</td>\n"+
-                "        <td >"+%LabViral%+"</td>\n"+
+                "        <td >"+"%LabCD4%"+"</td>\n"+
+                "        <td >"+"%LabHemoglobin%"+"</td>\n"+
+                "        <td >"+"%LabViral%"+"</td>\n"+
                 "    </tr>\n"+
                 "</table>\n";
 
