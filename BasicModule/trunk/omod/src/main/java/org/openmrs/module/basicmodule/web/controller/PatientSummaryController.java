b862ec1b5124d1fe649e106ea3454dce8a290c99
kay.n.choi <kay.n.choi@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-03-19 08:20:30 +0000

    Patient summary html code moved from patientsummarylink.jsp to PatientSummaryController.java.

--- BasicModule/trunk/omod/src/main/java/org/openmrs/module/basicmodule/web/controller/PatientSummaryController.java
+++ BasicModule/trunk/omod/src/main/java/org/openmrs/module/basicmodule/web/controller/PatientSummaryController.java
@@ -9,6 +9,11 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.PatientService;
+import org.openmrs.Encounter;
+import org.openmrs.api.EncounterService;
 
 @Controller
 @RequestMapping(value = "module/basicmodule/patientsummarylink.form")
@@ -21,8 +26,102 @@ public class PatientSummaryController {
 	private final String SUCCESS_FORM_VIEW = "/module/basicmodule/patientsummarylink";
 
     @RequestMapping(method = RequestMethod.GET)
-	public String showForm(){
-            System.out.println("PatientSummaryController showForm method***************");
+	public String showForm() {
+        System.out.println("PatientSummaryController showForm method***************");
 		return SUCCESS_FORM_VIEW;
 	}
+    
+    public String getHTML(String patientID) {
+        Patient p = PatientService.getPatient(method.valueOf("patientId"));
+        List<Encounter> encounters = Context.getEncounterService().getEncountersByPatient(p);
+        
+        String html = "<table width=100%>\n"+
+                "    <tr bgcolor='4D9999'>\n"+
+                "        <td colspan=2><font color='white'>Patient Information</font></td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td width=30% ><b>Name:</b> %patientName%</td>\n"+
+                "        <td width=70% align='right'><b>Clinic Location:</b> %patientCity%</td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td>%patientGender%, %patientAge% yrs<td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td colspan=2><center><b>Address:</b> %patientAddress%</center>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td colspan=2><center><b>Last seen on:</b> %LastSeen%</center>\n"+
+                "    </tr>\n"+
+                "</table>\n"+
+                "\n"+
+                "<p>\n"+
+                "\n"+
+                "<table width=100%>\n"+
+                "    <tr bgcolor='4D9999'>\n"+
+                "        <td colspan=2><font color='white'>Patient Summary</font></td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td width=30% ><b>WHO stage:</b> %patientWHOStage%</td>\n"+
+                "        <td width=70% ><b>Current art regimen drugs:</b> </td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td width=30% ><b>TB Status:</b> %patientWHOStage%</td>\n"+
+                "        <td width=70% rowspan='2'><b>%ARTRegimen%</b> </td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td width=30% ><b>Allergies:</b> %patientWHOStage%</td>\n"+
+                "    </tr>\n"+
+                "</table>\n"+
+                "\n"+
+                "<p>\n"+
+                "<br><br>\n"+
+                "\n"+
+                "<table width='100%' >\n"+
+                "    <tr bgcolor='4D9999'>\n"+
+                "        <td colspan='5' width='100%'><font color='white'>Patient Vitals</font></td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td ><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></td>\n"+
+                "        <td ><b>Weight (KG)</b></td>\n"+
+                "        <td ><b>Temperature (C)</b></td>\n"+
+                "        <td ><b>Blood Pressure</b></td>\n"+
+                "        <td ><b>Karnofsky Score</b></td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td >Enrollment: %enroll%</td>\n"+
+                "        <td >%enrollWt%</td>\n"+
+                "        <td >%enrollTemp%</td>\n"+
+                "        <td >%enrollBP%</td>\n"+
+                "        <td >%enrollKS%</td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td >Last Visit: %lastVisit%</td>\n"+
+                "        <td >%lastVisitWt%</td>\n"+
+                "        <td >%lastVisitTemp%</td>\n"+
+                "        <td >%lastVisitBP%</td>\n"+
+                "        <td >%lastVisitKS%</td>\n"+
+                "    </tr>\n"+
+                "</table>\n"+
+                "\n"+
+                "<p>\n"+
+                "<br><br>\n"+
+                "\n"+
+                "<table width='100%' >\n"+
+                "    <tr bgcolor='4D9999'>\n"+
+                "        <td colspan=3 width='100%'><font color='white'>Laboratory Results</font></td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td ><b>CD4</b></td>\n"+
+                "        <td ><b>Hemoglobin</b></td>\n"+
+                "        <td ><b>Viral Load</b></td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "        <td >%LabCD4%</td>\n"+
+                "        <td >%LabHemoglobin%</td>\n"+
+                "        <td >%LabViral%</td>\n"+
+                "    </tr>\n"+
+                "</table>\n";
+                
+        return html;
+    }
 }
