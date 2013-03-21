d5b3e16cd655e16a31946c3950159c21d7a6cf81
vwoeltjen@gmail.com <vwoeltjen@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-03-21 22:56:30 +0000

    Added comments to NamingContext explaining role of methods

--- Compiler/src/interpreter/NamingContext.java
+++ Compiler/src/interpreter/NamingContext.java
@@ -1,11 +1,31 @@
 package interpreter;
 
 /**
- *
+ * Represents a place where values may be stored and 
+ * retrieved by name. This may be the ExecutionContext 
+ * in general, or it may be a DSS object.
  * @author woeltjen
  */
 public interface NamingContext {
+    /**
+     * Get the value currently associated with 
+     * the specified name in this context
+     * @param name
+     * @return 
+     */
     public DSSValue get(String name);
+    
+    /**
+     * Set a name-value association in this context. 
+     * This may overwrite any previous association.
+     * @param name
+     * @param value 
+     */
     public void     set(String name, DSSValue value);
+    
+    /**
+     * All names used by objects in this context
+     * @return 
+     */
     public String[] names();
 }
