107971977329c4765dd76abfc87cc2d412e91404
kay.n.choi <kay.n.choi@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-03-23 08:00:44 +0000

    Added interpreter.DSSRead.java, interpreter.DSSReadInitialEncounter.java, interpreter.LatestEncounter.java. TODO: implement call() functions.

---
+++ Compiler/src/interpreter/DSSReadLatestEncounter.java
@@ -0,0 +1,21 @@
+package interpreter;
+
+/**
+ *  @author Kay Choi
+ */
+
+public class DSSReadLatestEncounter extends DSSFunction {
+    /**
+     * Call this function. The arguments provided are as observed by the
+     * interpreter. Note that the actual number of arguments may not match
+     * the number of parameters expected; it is ultimately the function's
+     * responsibility to handle this situation.
+     * @param args the arguments to the function
+     * @return the return value of the represented function
+     */
+    public DSSValue call(DSSValue... args) {
+        //DSSValue val = new DSSValue();
+        //return val;
+        return null;
+    }
+}
