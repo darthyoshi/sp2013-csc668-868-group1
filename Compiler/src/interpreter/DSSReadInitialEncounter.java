869b6576721fc57610794b99fe9ff65776590ce4
kay.n.choi <kay.n.choi@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-03-26 01:27:06 +0000

    implemented call() functions in DSSRead.java, DSSReadInitialEncounter.java, DSSReadLatestEncounter.java TODO: parse function arguments and retrieve patientId, conceptName TODO: convert Set object into DSSValue

--- Compiler/src/interpreter/DSSReadInitialEncounter.java
+++ Compiler/src/interpreter/DSSReadInitialEncounter.java
@@ -4,6 +4,17 @@ package interpreter;
  *  @author Kay Choi
  */
 
+import java.util.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.*;
+import org.openmrs.api.context.Context;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 public class DSSReadInitialEncounter extends DSSFunction {
     /**
      * Call this function. The arguments provided are as observed by the
@@ -14,8 +25,43 @@ public class DSSReadInitialEncounter extends DSSFunction {
      * @return the return value of the represented function
      */
     public DSSValue call(DSSValue... args) {
-        //DSSValue val = new DSSValue();
-        //return val;
+        //DSSValue result = new DSSValue();
+        
+        //TODO: parse args and retrieve patientId, conceptName
+        String patientID;
+        String conceptName;
+
+        List<Encounter> allEncounters = Context.getEncounterService().getEncountersByPatientId(Integer.parseInt(patientId));
+        Iterator<Encounter> encIter = allEncounters.iterator();
+        Encounter enc, initEnc;
+        Set<Obs> obs;
+        Iterator<Obs> obsIter;
+        Obs ob;
+        Set<Obs> validObs = new Set<Obs>();
+        Date initDate = null;
+        
+        while (encIter.hasNext()) {
+            enc = encIter.next();
+            
+            if (initDate == null || !initDate.before(enc.getEncounterDatetime())) {
+                initDate = enc.getEncounterDatetime();
+            
+                obs = enc.getAllObs();
+
+                obsIter = obs.iterator();
+                while (obsIter.hasNext()) {
+                    ob = obsIter.next();
+                    
+                    if (!ob.isVoided() && ob.getConcept.getName().getName().equals(conceptName)) {
+                        validObs.add(ob);
+                    }
+                }
+            }
+        }
+        
+        //TODO: convert Set to DSSValue
+        
+        //return result;
         return null;
     }
 }
--- Compiler/src/interpreter/DSSReadLatestEncounter.java
+++ Compiler/src/interpreter/DSSReadLatestEncounter.java
@@ -14,8 +14,43 @@ public class DSSReadLatestEncounter extends DSSFunction {
      * @return the return value of the represented function
      */
     public DSSValue call(DSSValue... args) {
-        //DSSValue val = new DSSValue();
-        //return val;
+        //DSSValue result = new DSSValue();
+        
+        //TODO: parse args and retrieve patientId, conceptName
+        String patientID;
+        String conceptName;
+
+        List<Encounter> allEncounters = Context.getEncounterService().getEncountersByPatientId(Integer.parseInt(patientId));
+        Iterator<Encounter> encIter = allEncounters.iterator();
+        Encounter enc, initEnc;
+        Set<Obs> obs;
+        Iterator<Obs> obsIter;
+        Obs ob;
+        Set<Obs> validObs = new Set<Obs>();
+        Date lastDate = null;
+        
+        while (encIter.hasNext()) {
+            enc = encIter.next();
+            
+            if (lastDate == null || !lastDate.after(enc.getEncounterDatetime())) {
+                lastDate = enc.getEncounterDatetime();
+            
+                obs = enc.getAllObs();
+
+                obsIter = obs.iterator();
+                while (obsIter.hasNext()) {
+                    ob = obsIter.next();
+                    
+                    if (!ob.isVoided() && ob.getConcept.getName().getName().equals(conceptName)) {
+                        validObs.add(ob);
+                    }
+                }
+            }
+        }
+        
+        //TODO: convert Set to DSSValue
+        
+        //return result;
         return null;
     }
 }
