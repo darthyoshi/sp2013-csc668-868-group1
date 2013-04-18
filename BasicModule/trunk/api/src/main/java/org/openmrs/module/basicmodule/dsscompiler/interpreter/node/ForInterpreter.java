376c5cac958997b1a8c83be9c90463976f584bc7
vwoeltjen@gmail.com <vwoeltjen@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-04-18 17:52:34 +0000

    Implemented 'for' control structure

--- BasicModule/trunk/api/src/main/java/org/openmrs/module/basicmodule/dsscompiler/interpreter/node/ForInterpreter.java
+++ BasicModule/trunk/api/src/main/java/org/openmrs/module/basicmodule/dsscompiler/interpreter/node/ForInterpreter.java
@@ -1,8 +1,12 @@
 package org.openmrs.module.basicmodule.dsscompiler.interpreter.node;
 
+import java.util.List;
+import org.openmrs.module.basicmodule.dsscompiler.ast.AST;
 import org.openmrs.module.basicmodule.dsscompiler.ast.ForTree;
+import org.openmrs.module.basicmodule.dsscompiler.ast.IdTree;
 import org.openmrs.module.basicmodule.dsscompiler.interpreter.ASTInterpreter;
 import org.openmrs.module.basicmodule.dsscompiler.interpreter.ExecutionContext;
+import org.openmrs.module.basicmodule.dsscompiler.value.DSSValue;
 import org.openmrs.module.basicmodule.dsscompiler.visitor.ASTVisitor;
 
 /**
@@ -11,6 +15,18 @@ import org.openmrs.module.basicmodule.dsscompiler.visitor.ASTVisitor;
  */
 public class ForInterpreter implements ASTInterpreter<ForTree> {
     public Object interpret(ForTree tree, ExecutionContext context, ASTVisitor visitor) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        AST leftKid = tree.getKid(1);
+        if (leftKid instanceof IdTree) {
+            String id = ((IdTree) leftKid).getSymbol().toString();
+            List list = context.getEvaluator().
+                    castTo(List.class, (DSSValue) tree.getKid(2).accept(visitor));
+            if (list != null) {
+                for (Object obj : list) {
+                    context.set(id, context.getEvaluator().toDSSValue(obj));
+                    tree.getKid(3).accept(visitor);
+                }
+            }
+        }
+        return null;
     }    
 }
