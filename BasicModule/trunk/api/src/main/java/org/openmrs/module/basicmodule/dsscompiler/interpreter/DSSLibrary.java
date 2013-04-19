d741f987210616572f337cbd3b83144a9b8580ab
vwoeltjen@gmail.com <vwoeltjen@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-04-19 16:47:40 +0000

    Added comments to DSSLibrary clarifying usage

--- BasicModule/trunk/api/src/main/java/org/openmrs/module/basicmodule/dsscompiler/interpreter/DSSLibrary.java
+++ BasicModule/trunk/api/src/main/java/org/openmrs/module/basicmodule/dsscompiler/interpreter/DSSLibrary.java
@@ -13,6 +13,10 @@ public interface DSSLibrary {
      * Get all functions defined in this library. These should be returned 
      * in a map from function name -> function object.
      * 
+     * Function name should be the name used to call the function from a DSS 
+     * program (e.g. "read"); function object should be a Java object that 
+     * extends DSSFunction (e.g. new DSSRead())
+     * 
      * An ExecutionContext is provided in case a library needs to use or 
      * link against elements in that environment. DSSLibrary implementations 
      * are free to ignore this argument.
