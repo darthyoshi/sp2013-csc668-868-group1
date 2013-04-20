b192f707266041d87a9a732ffd2b5e688ad37e37
CALiforykng5i5@gmail.com <CALiforykng5i5@gmail.com@8f292820-0a9e-4756-579c-e4d96cb42ef5>
2013-04-20 00:00:34 +0000

    Fix the DSSOldestTimeItem and DSSRecentTimeItem
    Taking victor's advice 
    
    Args[0] is a  DSSValueList
    
    I used get(i) to retrieve a DSSValue for the index. get(++i) to get the next DSSValue item.
    
    I used getTimeStamp() method on both DSSValue  to convert them to Date type
    
    By comparing each items with Date type 's compare() method.
    
    kent

--- BasicModule/trunk/api/src/main/java/org/openmrs/module/basicmodule/dsscompiler/interpreter/instrinsics/date/DSSOldestTimeItem.java
+++ BasicModule/trunk/api/src/main/java/org/openmrs/module/basicmodule/dsscompiler/interpreter/instrinsics/date/DSSOldestTimeItem.java
@@ -8,36 +8,47 @@ import java.util.Date;
 import org.openmrs.module.basicmodule.dsscompiler.interpreter.DSSFunction;
 import org.openmrs.module.basicmodule.dsscompiler.value.DSSValue;
 import org.openmrs.module.basicmodule.dsscompiler.value.DSSValueDate;
+import org.openmrs.module.basicmodule.dsscompiler.value.DSSValueList;
 
 /**
- * oldestTimeItem(list) – return the item with the oldest time
- * The display format I used will match the system format.
- * Default format yyyy-MM-dd hh:mm:ss   
- * e.g.
- * Name	Value
- * System Date	2013-04-11 
- * System Time  09:07:32 
+ * oldestTimeItem(list) – return the item with the oldest time The display
+ * format I used will match the system format. Default format yyyy-MM-dd
+ * hh:mm:ss e.g. Name	Value System Date	2013-04-11 System Time 09:07:32
+ *
  * @author kent
  */
-public class DSSOldestTimeItem extends DSSFunction{
+public class DSSOldestTimeItem extends DSSFunction {
     /* 
-     * Iterate through the whole array of DSSValu and return the 
+     * Iterate through the whole array of DSSValue and return the 
      * oldestItem by comparing the DSSValue.
      * @param DSSValue... args
      * @return DSSValueDate the oldestItem 
      */
-     public DSSValue call(DSSValue... args){
-         Date date = new Date();
-         DSSValue oldestItem = new DSSValueDate(date) ;
-        
-         //Set<DSSValue> = args;
-         int i;
-         int max =args.length -1;
-        for(i=0; i< max ;i++){
-            if (args[i].lessthanequal(args[++i])) {oldestItem = args[i] ;}
-            else { oldestItem = args[++i];}
+
+    public DSSValue call(DSSValue... args) {
+        Date date = new Date();
+        DSSValue oldestItem = new DSSValueDate(date);
+
+        //dssDateList will hold all DSSValue Date 
+        DSSValueList dssDateList = (DSSValueList) args[0];
+
+        int i;
+        int max = dssDateList.length();
+        for (i = 0; i < max; i++) {
+            // get(i) return a DSSValue from the DSSValuDate list
+            // getTimeStamp return a Date type of the item
+            // Compare each DSSValueDate as Date type to find the oldest
+            // If it is less than and equal to 0, oldestItem is the same as current item 
+            // otherwise, it gets assgined to next item
+            if (dssDateList.get(i).getTimeStamp().compareTo(dssDateList.get(++i).getTimeStamp()) <= 0) {
+                oldestItem = dssDateList.get(i);
+            } else {
+                oldestItem = dssDateList.get(++i);
+            }
         }
+
+
         return oldestItem;
-       
+
     }
 }
